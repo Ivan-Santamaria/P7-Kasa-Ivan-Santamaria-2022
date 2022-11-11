4f53dedc2fa1cb251e4c69c2822f67de94907d8f
Ivan-Santamaria <81047384+Ivan-Santamaria@users.noreply.github.com>
2022-11-11 21:02:28 +0100

    A Propos: importation des description depuis .json

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,7 @@
 import { Banner, Accordion } from "../Routes/ComponentsRoutes";
 import "../styles/Pages/About.css";
+// Importation du fichier json contenant les information de la page Apropos
+const aboutData = require("../datas/aboutData.json");
 
 export default function About() {
   return (
@@ -8,7 +10,8 @@ export default function About() {
         <Banner />
       </div>
       <div className="accordion-about-container">
-        <Accordion title="test2" content="test3" />
+        {/* Récupération des éléments de aboutData.json et injection dans le titre et le contenu de l'accordéon */}
+        <Accordion title={aboutData[0].title} />
       </div>
     </div>
   );
