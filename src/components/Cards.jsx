8eca4147514f30028e23766ac0edcc5b707ea1ff
Ivan-Santamaria <81047384+Ivan-Santamaria@users.noreply.github.com>
2022-11-11 21:16:54 +0100

    Home: Card: Récupération des titres depuis .json

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,15 +3,22 @@ import { Link } from "react-router-dom";
 import "../styles/Components/Cards.css";
 
 export default function Cards(props) {
+  // Importation des données de logements sur le fichier .json
+  const annonces = require("../datas/logements.json");
+
   return (
     <div className="div-cards">
-      <Link className="links">
-        <ul className="cards card-home">
-          <li className="li">
-            <div className="text-shadow">{}</div>
-          </li>
-        </ul>
-      </Link>
+      {/* bouclage avec .map() pour afficher les annonces grâce a l'id
+      ainsi que la photo de couverture de l'annonce et son titre */}
+      {annonces.map((annonce) => (
+        <Link className="links">
+          <ul className="cards card-home">
+            <li className="li">
+              <div className="text-shadow">{annonce.title}</div>
+            </li>
+          </ul>
+        </Link>
+      ))}
     </div>
   );
 }
