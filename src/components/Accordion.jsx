c530ed9aa891c535f3b90ed98cc35c025f120d38
Ivan-Santamaria <81047384+Ivan-Santamaria@users.noreply.github.com>
2022-11-11 21:04:17 +0100

    A Propos: utilisation du state pour l'accordéon

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,14 +4,33 @@ import { HiChevronDown, HiChevronUp } from "react-icons/hi";
 import "../styles/Components/Accordion.css";
 
 export default function Accordion(props) {
+  // Le contenu de l'accordéon est invisible par défault
+  const [visible, setVisibility] = useState(false);
+
+  // Récupère le contenu des Accordéons grâce aux props
+  const contentType = typeof props.content;
+
   return (
     <div className="accordion-item">
-      <div className="accordion-flexbox">
+      {/* Au clic le contenu devient visible */}
+      <div
+        className="accordion-flexbox"
+        // Changement de l'état de l'accordéon: visible || caché
+        // Si Caché au click => déplois les informations
+        onClick={() => setVisibility(!visible)}
+      >
         <p className="accordion-title">{props.title}</p>
         <p className="accordion-icon">
-          <HiChevronDown /> : <HiChevronUp />
+          {visible ? <HiChevronDown /> : <HiChevronUp />}
         </p>
       </div>
+      {visible && (
+        <div className="accordion-collapse-text-container">
+          {contentType === "string" && (
+            <p className="collapsed-text">{props.content}</p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
