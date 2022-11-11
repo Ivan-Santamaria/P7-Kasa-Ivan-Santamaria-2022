6fa51e21a72a060db6be164968402148533e72f3
Ivan-Santamaria <81047384+Ivan-Santamaria@users.noreply.github.com>
2022-11-11 21:05:44 +0100

    A Propos: Accordéon 4 volets

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -11,7 +11,10 @@ export default function About() {
       </div>
       <div className="accordion-about-container">
         {/* Récupération des éléments de aboutData.json et injection dans le titre et le contenu de l'accordéon */}
-        <Accordion title={aboutData[0].title} />
+        <Accordion title={aboutData[0].title} content={aboutData[0].content} />
+        <Accordion title={aboutData[1].title} content={aboutData[1].content} />
+        <Accordion title={aboutData[2].title} content={aboutData[2].content} />
+        <Accordion title={aboutData[3].title} content={aboutData[3].content} />
       </div>
     </div>
   );
