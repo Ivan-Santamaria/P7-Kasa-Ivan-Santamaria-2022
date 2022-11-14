d69b9b5298e089b376bac9543eae7357c6b8ee7d
Ivan-Santamaria <81047384+Ivan-Santamaria@users.noreply.github.com>
2022-11-14 15:17:37 +0100

    Fiche Logement: IMportation de l'accordéon

--- src/Pages/Lodging.jsx
+++ src/Pages/Lodging.jsx
@@ -1,7 +1,23 @@
+
+
+import { Accordion } from "../Routes/ComponentsRoutes";
+
+
 export default function Lodging() {
   return (
     <div className="body">
-      <p>test</p>
+      {/* Grâce au code précedent {annonces.pictures}, 
+      {annonce.title}, etc on récupère les photos de l'annonce 
+      son titre sa localisation, etc... */}
+      <div className="div-accordion">
+        {/* Avec title="..." je nomme mon collapse 
+        car si je le fais directement sur le fichier collapse alors 
+        tous les collapse utilisés se nomeront description 
+        ou équipements par exemple Collapse 
+        est un composant utilisé également dans le fichier A propos*/}
+        <Accordion title="Description" content={"test"} />
+        <Accordion title="Equipements" content={"test2"} />
+      </div>
     </div>
   );
 }
