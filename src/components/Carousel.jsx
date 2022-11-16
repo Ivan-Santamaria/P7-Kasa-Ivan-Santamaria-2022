c2219134e5d230fd21d886c929420b12ff52c94d
Ivan-Santamaria <81047384+Ivan-Santamaria@users.noreply.github.com>
2022-11-16 16:21:34 +0100

    Fiche Logement: Carousel Importation des chevrons

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,16 +1,27 @@
 import "../styles/Components/Carousel.css";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
 export default function Carousel(props) {
   return (
     <div className="carousel-container">
-      {props.pictures.map((picture) => (
-        <img
-          className="carousel-container-img"
-          alt="carousel"
-          src={picture}
-          key={picture}
-        />
-      ))}
+      <div className="carousel">
+        {props.pictures.map((picture) => (
+          <img
+            className="carousel-container-img"
+            alt="carousel"
+            src={picture}
+            key={picture}
+          />
+        ))}
+      </div>
+      <div className="carousel-controls">
+        <span className="react-icon react-icon-chevron">
+          <HiChevronLeft />
+        </span>
+        <span className="react-icon span react-icon-chevron">
+          <HiChevronRight />
+        </span>
+      </div>
     </div>
   );
 }
