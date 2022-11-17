788c7f89fdcdb0571dee0271a62d6431abd4f784
Ivan-Santamaria <81047384+Ivan-Santamaria@users.noreply.github.com>
2022-11-17 17:40:10 +0100

    Fiche Logement: Carousel <===>,  !!Affichage!!

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,24 +1,56 @@
 import "../styles/Components/Carousel.css";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+import { useState } from "react";
 
 export default function Carousel(props) {
+  const [slideIdx, setCurrentSlide] = useState(0);
+  const imgSize = () => {
+    const sliderImg = document.querySelector(".carousel-container img");
+    if (!sliderImg) {
+      return 0;
+    }
+    return sliderImg.width;
+  };
+
+  const onNext = () => {
+    if (slideIdx === props.pictures.length - 1) {
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide(slideIdx + 1);
+    }
+  };
+
+  const onPrev = () => {
+    if (slideIdx === 0) {
+      setCurrentSlide(props.pictures.length - 1);
+    } else {
+      setCurrentSlide(slideIdx - 1);
+    }
+  };
+
   return (
     <div className="carousel-container">
-      <div className="carousel">
+      <div
+        className="carousel"
+        style={{
+          transform: `translateX(-${slideIdx * imgSize()}px)`,
+        }}
+      >
+        {/* On boucle sur les images */}
         {props.pictures.map((picture) => (
           <img
-            className="carousel-container-img"
-            alt="carousel"
+            className="slider-container-img"
+            alt="slider"
             src={picture}
             key={picture}
           />
         ))}
       </div>
       <div className="carousel-controls">
-        <span className="react-icon react-icon-chevron">
+        <span className="react-icon react-icon-chevron" onClick={onPrev}>
           <HiChevronLeft />
         </span>
-        <span className="react-icon span react-icon-chevron">
+        <span className="react-icon span react-icon-chevron" onClick={onNext}>
           <HiChevronRight />
         </span>
       </div>
