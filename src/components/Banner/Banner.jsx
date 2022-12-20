196284161297a1f7729c565a4bd64257d495918e
Ivan-Santamaria <81047384+Ivan-Santamaria@users.noreply.github.com>
2022-12-20 17:42:26 +0100

    Modifications mineures Banière

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,10 +4,8 @@ import "./Banner.css";
 export default function Banner(props) {
   return (
     <div className="card div-card">
-      <div className="div-container">
-        {/* Récupération du contenu grâce aux props */}
-        <p className="home-paragraph container">{props.content}</p>
-      </div>
+      {/* Récupération du contenu grâce aux props */}
+      <p className="home-paragraph container">{props.content}</p>
     </div>
   );
 }
